bd612e6f53c7490049f3cc5f4b62bde33e631d97
Kayan Almeida <kayant.almeida@gmail.com>
2017-11-14 19:35:52 -0200

    Added patient exam number

--- patient/forms.py
+++ patient/forms.py
@@ -1,5 +1,7 @@
 from django.forms import ModelForm
 from .models import Patient
+from django.core.validators import EMPTY_VALUES
+
 
 class PatientForm(ModelForm):
 
@@ -7,9 +9,25 @@ class PatientForm(ModelForm):
         model = Patient
         exclude = ('created_at',)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # for key in self.fields:
-            # print(self.fields[key].widget.help_text)
+    def clean(self):
+        has_referral = self.cleaned_data.get('has_referral', False)
+        print(has_referral)
+        if has_referral:
+            print(self.cleaned_data)
+            referral_character = self.cleaned_data.get(
+                'referral_character', None)
+            other_referral_place = self.cleaned_data.get(
+                'other_referral_place', None)
+
+            if referral_character in EMPTY_VALUES:
+                self._errors['referral_character'] = self.error_class([
+                    'Caráter do encaminhamento obrigatório'])
+
+            if other_referral_place in EMPTY_VALUES:
+                self._errors['other_referral_place'] = self.error_class([
+                    'Local do encaminhamento obrigatório'])
+        print(self._errors)
+        return self.cleaned_data
+
 
 
--- patient/models.py
+++ patient/models.py
@@ -11,26 +11,38 @@ class Patient(models.Model):
     address = models.CharField(max_length=300, verbose_name="Endereço")
     phone = models.CharField(max_length=100, verbose_name="Telefone")
     birthdate = models.DateField(verbose_name="Data de nascimento")
+    exame_number = models.CharField(
+        verbose_name="Número do exame laboratorial", max_length=40,
+        null=True, blank=True)
     notes = models.CharField(
-        max_length=300, verbose_name="Observações", null=True)
+        max_length=300, verbose_name="Observações", null=True, blank=True)
     prostate_history = models.NullBooleanField(
         verbose_name="Tem alguém na família com problemas na próstata")
     trouble_urinating = models.NullBooleanField(
         verbose_name="Problemas para urinar?")
     suspicious_nodule = models.NullBooleanField(
         verbose_name="Possui nódulo suspeito?")
-    has_referral = models.NullBooleanField(verbose_name="Encaminhamento")
+
+    has_referral = models.NullBooleanField(
+        verbose_name="Encaminhamento", default=False)
 
     referral_character = models.CharField(
         max_length=100,
         verbose_name="Caráter",
+        null=True,
+        blank=True,
         choices=[(str(i), CHARACTER_CHOICES[i]) for i in range(len(CHARACTER_CHOICES))])
 
     referral_place = models.CharField(
         max_length=100,
         verbose_name="Local de encaminhamento",
+        null=True,
+        blank=True,
         choices=[(str(i), REFERAL_PLACES[i]) for i in range(len(REFERAL_PLACES))])
-    other_referral_place = models.CharField(max_length=100)
+
+    other_referral_place = models.CharField(
+        max_length=100, verbose_name="Especificar local",
+        null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
