64403e17c5b5d9390dd39dbbc70e6be270fe07f7
Kayan Almeida <kayant.almeida@gmail.com>
2017-11-14 13:40:06 -0200

    Fixing views

--- patient/views.py
+++ patient/views.py
@@ -5,6 +5,9 @@ from .models import Patient
 from .forms import PatientForm
 
 
+def get_registry_number():
+    return Patient.objects.count() + 1
+
 def index(request):
     patient_list = Patient.objects.all()
     paginator = Paginator(patient_list, 25)
@@ -17,16 +20,19 @@ def index(request):
         patients = paginator.page(paginator.num_pages)
     return render(request, 'patient/list.html', {'patients': patients})
 
+
 def create(request):
     title = 'Novo paciente'
     if request.method == 'GET':
         return render(
             request, 'patient/view.html',
-            { 'title': title, 'form': PatientForm() })
+            {
+                'title': title,
+                'form': PatientForm(), 'number': get_registry_number(),
+            })
     else:
         form = PatientForm(request.POST)
         if form.is_valid():
-            print("valid")
             patient = form.save()
             return redirect('patient:list')
         else:
@@ -34,17 +40,28 @@ def create(request):
             request, 'patient/view.html',
             { 'title': title, 'form': form })
 
+
 def edit(request, patient_id):
+    patient = get_object_or_404(Patient, pk=patient_id)
+    form = PatientForm(instance=patient)
     if request.method == 'GET':
-        patient = get_object_or_404(Patient, pk=patient_id)
-        form = PatientForm(instance=patient)
-        if form.is_valid():
-            patient = form.save(commit=True)
         return render(
             request, 'patient/view.html',
-            { 'title': 'Paciente', 'patient': patient, 'form': form})
+            {
+                'title': 'Paciente',
+                'patient': patient, 'form': form, 'number': patient.id
+            })
     else:
-        pass
+        form = PatientForm(request.POST, instance=patient, )
+        if form.is_valid():
+            patient.save()
+            return redirect('patient:list')
+        else:
+            print(form.errors)
+            return render(
+            request, 'patient/view.html',
+            { 'title': 'Paciente', 'patient': patient, 'form': form})
+
 
 def delete(request):
     pass
