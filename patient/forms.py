bf7e20ade0367e9e422ef273b00b01fcaaaf06f2
Kayan Almeida <kayant.almeida@gmail.com>
2017-11-16 17:53:08 -0200

    Fixing form validation

--- patient/forms.py
+++ patient/forms.py
@@ -11,22 +11,23 @@ class PatientForm(ModelForm):
 
     def clean(self):
         has_referral = self.cleaned_data.get('has_referral', False)
-        print(has_referral)
         if has_referral:
-            print(self.cleaned_data)
             referral_character = self.cleaned_data.get(
                 'referral_character', None)
-            other_referral_place = self.cleaned_data.get(
-                'other_referral_place', None)
-
             if referral_character in EMPTY_VALUES:
                 self._errors['referral_character'] = self.error_class([
                     'Caráter do encaminhamento obrigatório'])
-
-            if other_referral_place in EMPTY_VALUES:
+            referral_place = self.cleaned_data.get('referral_place', None)
+            other_referral_place = self.cleaned_data.get(
+                'other_referral_place', None)
+            if referral_place in EMPTY_VALUES:
+                self._errors['referral_place'] = self.error_class([
+                        'Local do encaminhamento obrigatório'])
+            elif int(referral_place) == 2 and \
+                    other_referral_place in EMPTY_VALUES:
                 self._errors['other_referral_place'] = self.error_class([
                     'Local do encaminhamento obrigatório'])
-        print(self._errors)
+
         return self.cleaned_data
 
 
